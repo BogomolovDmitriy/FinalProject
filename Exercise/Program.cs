218c7d960c6d2116f65746dc1c1c41d78eb9a938
Dmitriy <dimgod80@yandex.ru>
2022-12-04 02:23:03 +0800

    add GetArrayItemLengthMoreThanThree

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayA = {"Hello!", "2", "World", ":-)"};
+
+string[] GetArrayItemLengthMoreThanThree(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count ++;
+        }
+    }
+    string[] tempArray = new string[count]; 
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            tempArray[j] = array[i];
+            j ++;
+        }
+    }
+    return tempArray;  
+}
